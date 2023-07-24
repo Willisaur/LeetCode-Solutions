021fd1f0d20e94491eb60024967a3679ad171c9a
Will Starling <willstar2003@gmail.com>
2023-07-24 14:56:39 -0500

    C++ Solution
                Submission Statistics:
                Question #: 58
                Language: C++
                Runtime: 0 ms
                Runtime percentile: 100.00
                Memory: 6.608 MB
                Memory percentile: 11.43

---
+++ 58. Length of Last Word/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        bool wasSpace = false;
+
+        for (int i=0; i<s.length(); ++i){
+            if (static_cast<int>(s[i]) != 32){ // not a space
+                if (wasSpace){
+                    length = 0;
+                    wasSpace = false;
+                }
+                length += 1;
+                
+            } else {
+                wasSpace = true;
+            }
+        }
+
+        return length;
+    }
+};
