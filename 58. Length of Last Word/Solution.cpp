88c6b33657813b4a4299d39100b756f7eb8c0643
Will Starling <willstar2003@gmail.com>
2023-07-24 15:04:16 -0500

    C++ Solution
                Submission Statistics:
                Question #: 58
                Language: C++
                Runtime: 0 ms
                Runtime percentile: 100.00
                Memory: 6.46 MB
                Memory percentile: 84.55

--- 58. Length of Last Word/Solution.cpp
+++ 58. Length of Last Word/Solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length = 0;
-        bool wasSpace = false;
+        // iterate backwards since everything but the last word and surrounding whitespace is irrelevant
+        // have a boolean which tracks whether a character has been counted yet
+        // when the first character is counted, the bool switches states
+        // the next whitespace causes a return of the length
 
-        for (int i=0; i < s.length(); ++i){
-            if (s[i] != 32){ // not a space
-                if (wasSpace){
-                    length = 0;
-                    wasSpace = false;
-                }
+        int length = 0;
+        bool firstChar = false;
+        int i = s.length();
+
+        while (--i >= 0){
+            if (s[i] != 32){
+                firstChar = true;
                 length += 1;
-                
-            } else {
-                wasSpace = true;
+            } else if (firstChar){
+                return length;
             }
         }
 
