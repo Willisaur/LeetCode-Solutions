4c96f47ada39f58cb823ae816725d0c0a0df6ce9
Will Starling <willstar2003@gmail.com>
2023-07-25 00:33:03 -0500

    Python3 Solution
                Submission Statistics:
                Question #: 383
                Language: Python3
                Runtime: 83 ms
                Runtime percentile: 44.50
                Memory: 16.636 MB
                Memory percentile: 30.76

---
+++ 383. Ransom Note/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magDict = dict()
+
+        for ch in magazine:
+            if ch in magDict:
+                magDict[ch] += 1
+            else:
+                magDict[ch] = 1
+        
+        for ch in ransomNote:
+            if ch not in magDict.keys() or magDict[ch] == 0:
+                return False
+            else:
+                magDict[ch] -= 1
+        
+        return True
