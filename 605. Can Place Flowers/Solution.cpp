dd8eb29347c5ef8fa182df06f2362072ea2057e7
Will Starling <willstar2003@gmail.com>
2023-08-01 17:42:19 -0500

    C++ Solution
                Submission Statistics:
                Question #: 605
                Language: C++
                Runtime: 195 ms
                Runtime percentile: 5.78
                Memory: 20.324 MB
                Memory percentile: 23.51

--- 605. Can Place Flowers/Solution.cpp
+++ 605. Can Place Flowers/Solution.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        // you only need to look at the first two elements since they're the edge
+        // if elements 1 and 2 are 0, you can plant a flower
+        // if element 1 is 1, you cannot plant a flower in the current two spots
+        // if element 2 is 1, you cannot plant a flower in the current 2 + 1 spots
+        // if one element is left over and it's a zero, you can plant a flower
+        auto start  = flowerbed.begin();
         while (flowerbed.size() > 1){
-            auto start = flowerbed.begin();
             if (flowerbed[0] == 0 && flowerbed[1] == 0){
                 flowerbed.erase(start, start + 2);
                 --n;
@@ -11,6 +16,7 @@ public:
             } else {
                 flowerbed.erase(start, start + (flowerbed.size() > 2 ? 3 : 2));
             }
+            start = flowerbed.begin();
         }
         if (flowerbed.size() == 1 && !flowerbed[0]){
             --n;
