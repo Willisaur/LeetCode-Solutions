12e205c433f204b4a5ac7c7b61a56d78ff314160
Will Starling <willstar2003@gmail.com>
2023-08-16 18:11:32 -0500

    Pandas Solution
            Submission Statistics:
            Question #: 586
            Language: Pandas
            Runtime: 260 ms
            Runtime percentile: 74.70
            Memory: 60.968 MB
            Memory percentile: 46.29

--- 586. Customer Placing the Largest Number of Orders/Solution.py
+++ 586. Customer Placing the Largest Number of Orders/Solution.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
 
-    return pd.DataFrame({"customer_number": orders["customer_number"].mode()})
+    orders_counts = orders.groupby("customer_number", as_index=False)["order_number"].count()
+    customer_max = orders_counts[orders_counts["order_number"] == orders_counts["order_number"].max()]["customer_number"]
+
+    return pd.DataFrame({"customer_number": customer_max})
