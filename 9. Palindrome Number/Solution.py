dfdd9d305db577ab8d623198e8f733c685721f3c
Will Starling <willstar2003@gmail.com>
2023-09-21 14:21:34 -0400

    Python3 Solution
            Submission Statistics:
            Question #: 9
            Language: Python3
            Runtime: 77 ms
            Runtime percentile: 13.28
            Memory: 16.148 MB
            Memory percentile: 92.03

--- 9. Palindrome Number/Solution.py
+++ 9. Palindrome Number/Solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+        if x<0:
+            return False
+        temp = x
+        rev = 0
+        while temp > 0:
+            digit = temp % 10
+            temp //= 10
+            rev = rev * 10 + digit
+        return x==rev
