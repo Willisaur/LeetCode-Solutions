037ea5f6d3c984b720d79cb4e8378bf860aef620
Will Starling <willstar2003@gmail.com>
2023-10-04 16:50:00 -0400

    C++ Solution
            Submission Statistics:
            Question #: 917
            Language: C++
            Runtime: 64 ms
            Runtime percentile: 42.03
            Memory: 42.192 MB
            Memory percentile: 29.61

---
+++ 917. Boats to Save People/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int i = 0;
+        int count = 0;
+        int j = people.size() - 1;
+
+        while (i < j){
+            if (people[i] + people[j] <= limit){
+                ++i;
+                ++count;
+            } 
+            else if (people[j] <= limit){
+                ++count;
+            }
+            --j;
+        }
+
+        if (i == j && people[i] <= limit){
+            ++count;
+        }
+
+        return count;
+    }
+};
