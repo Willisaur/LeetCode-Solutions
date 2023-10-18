51ef95b7946d029faf0e81cfb45be599cb1b2d51
Will Starling <willstar2003@gmail.com>
2023-10-17 23:33:11 -0400

    Python3 Solution
            Submission Statistics:
            Question #: 62
            Language: Python3
            Runtime: 35 ms
            Runtime percentile: 78.68
            Memory: 16.336 MB
            Memory percentile: 36.96

--- 62. Unique Paths/Solution.py
+++ 62. Unique Paths/Solution.py
@@ -3,8 +3,6 @@ class Solution:
         # m = # of rows
         # n = # of columns
         matrix = [[0 for x in range(n)] for x in range(m)]
-        matrix[-1][-1] = 1
-        print(matrix)
 
         # return the number of paths from the right square to the end and the below square to the end
         def checkPaths(i, j):
@@ -12,9 +10,9 @@ class Solution:
                 return 1  # set the finish to 1 path
 
             accum = 0
-            if i < m-1:
+            if i < m-1: # don't access a row out of bounds
                 accum += matrix[i+1][j]
-            if j < n-1:
+            if j < n-1: # don't access a column out of bounds
                 accum += matrix[i][j+1]
             return accum
 
