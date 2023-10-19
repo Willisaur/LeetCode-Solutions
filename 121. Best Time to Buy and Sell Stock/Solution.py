754f2fc130bf57b00988470faba77160c22b8894
Will Starling <willstar2003@gmail.com>
2023-10-19 17:30:39 -0400

    Python3 Solution
            Submission Statistics:
            Question #: 121
            Language: Python3
            Runtime: 777 ms
            Runtime percentile: 97.08
            Memory: 27.26 MB
            Memory percentile: 74.83

--- 121. Best Time to Buy and Sell Stock/Solution.py
+++ 121. Best Time to Buy and Sell Stock/Solution.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # if only one day occurs, we cannot profit
         if len(prices) == 1:
             return 0
-
-        # we will compare the current stock price to the lowest price yet
-        maxProfit = 0 # running max profit counter
-        lowest = prices[0] 
+        
+        lowest = prices[0]
+        maxProfit = 0
 
         for price in prices:
             if price < lowest:
                 lowest = price
-            else: 
-                if maxProfit < price - lowest:
+            else:
+                if price - lowest > maxProfit:
                     maxProfit = price - lowest
-        
+
         return maxProfit
