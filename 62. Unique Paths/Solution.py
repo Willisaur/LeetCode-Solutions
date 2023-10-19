9edf89623df2daf933b1699cc0aaa738cc5bb683
Will Starling <willstar2003@gmail.com>
2023-10-19 17:51:09 -0400

    Python3 Solution
            Submission Statistics:
            Question #: 62
            Language: Python3
            Runtime: 37 ms
            Runtime percentile: 69.14
            Memory: 16.304 MB
            Memory percentile: 36.40

--- 62. Unique Paths/Solution.py
+++ 62. Unique Paths/Solution.py
@@ -1,23 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # m = # of rows
-        # n = # of columns
-        matrix = [[0 for x in range(n)] for x in range(m)]
+        # m = number of rows
+        # n = number of columns
 
-        # return the number of paths from the right square to the end and the below square to the end
+        grid = [[0 for x in range(n)] for x in range(m)]
+
+        # return the number of paths from the right cell to the end and below cell to the end
         def checkPaths(i, j):
-            if i+1 == m and j+1 == n: 
-                return 1  # set the finish to 1 path
-
-            accum = 0
-            if i < m-1: # don't access a row out of bounds
-                accum += matrix[i+1][j]
-            if j < n-1: # don't access a column out of bounds
-                accum += matrix[i][j+1]
+            # base case
+            # if at the bottom right corner, return 1
+            if i + 1 == m and j + 1 == n:
+                return 1
+            
+            accum = 0 # number of paths
+            # don't go out of bounds
+            if i < m-1: # columns
+                accum += grid[i+1][j]
+            if j < n-1: # rows
+                accum += grid[i][j+1]
             return accum
-
+        
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
-                matrix[i][j] = checkPaths(i, j)
+                grid[i][j] = checkPaths(i, j)
 
-        return matrix[0][0]
+        return grid[0][0]
