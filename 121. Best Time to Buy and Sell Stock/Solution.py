8486024d420ee11bb53041851f42507269d979a1
Will Starling <willstar2003@gmail.com>
2023-10-19 23:25:18 -0400

    Python3 Solution
            Submission Statistics:
            Question #: 121
            Language: Python3
            Runtime: 784 ms
            Runtime percentile: 95.03
            Memory: 27.268 MB
            Memory percentile: 74.83

--- 121. Best Time to Buy and Sell Stock/Solution.py
+++ 121. Best Time to Buy and Sell Stock/Solution.py
@@ -1,16 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # cannot profit with only 1 day
         if len(prices) == 1:
             return 0
         
+        # the lowest value will help calculate the max profit possible
         lowest = prices[0]
+        # running highest-profit counter
         maxProfit = 0
 
+        # for-each loop can be used since index is irrelevant
         for price in prices:
+            # if there is a new low price, update the lowest price
             if price < lowest:
                 lowest = price
-            else:
+            else: # price >= lowest; profit can be made
+                # if the amount of profit is the new max, update the new max
                 if price - lowest > maxProfit:
                     maxProfit = price - lowest
 
-        return maxProfit
+        return maxProfit # return max profit
