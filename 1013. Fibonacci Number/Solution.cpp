053c85cfce59e545c748ec799d1626d360016e01
Will Starling <willstar2003@gmail.com>
2023-10-19 23:25:40 -0400

    C++ Solution
            Submission Statistics:
            Question #: 1013
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 6.392 MB
            Memory percentile: 24.93

--- 1013. Fibonacci Number/Solution.cpp
+++ 1013. Fibonacci Number/Solution.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> dp{0, 1};
+        // fib(0) = 0
+        // fib(1) = 1
+        // fib(2) = 1
+        vector<int> dp{0, 1}; // initialize vector with values 0 and 1/ terms 0 and 1
 
-        for (int i = 2; i <=n ; ++i){
-            dp.push_back(dp[i-1] + dp[i-2]);
+        for (int i = 2; i <=n ; ++i){ // any terms for n >= 2
+            dp.push_back(dp[i-1] + dp[i-2]); // add to the vector the sum of the last 2 fib values
         }
 
-        return dp[n];
+        return dp[n]; // return fib(n)
     }
 };
