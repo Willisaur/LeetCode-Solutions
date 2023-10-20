e6bcaa351745b829484d33f5d88fbabfed5efa27
Will Starling <willstar2003@gmail.com>
2023-10-19 23:25:05 -0400

    Python3 Solution
            Submission Statistics:
            Question #: 62
            Language: Python3
            Runtime: 42 ms
            Runtime percentile: 32.61
            Memory: 16.108 MB
            Memory percentile: 88.94

--- 62. Unique Paths/Solution.py
+++ 62. Unique Paths/Solution.py
@@ -3,25 +3,32 @@ class Solution:
         # m = number of rows
         # n = number of columns
 
-        grid = [[0 for x in range(n)] for x in range(m)]
+        grid = [[0 for x in range(n)] for x in range(m)] # dual list comprehension
+        """
+        for a 3x4 (m x n):
+        [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
+        """
 
-        # return the number of paths from the right cell to the end and below cell to the end
+        # return the number of unique paths in the right cell and below cell
         def checkPaths(i, j):
             # base case
-            # if at the bottom right corner, return 1
-            if i + 1 == m and j + 1 == n:
+            # if at the start/top left corner, return 1
+            if j == 0 and i == 0:
                 return 1
             
-            accum = 0 # number of paths
-            # don't go out of bounds
-            if i < m-1: # columns
-                accum += grid[i+1][j]
-            if j < n-1: # rows
-                accum += grid[i][j+1]
+            accum = 0 # number of unique paths to this cell
+            # out-of-bounds checking
+            if i < m: # columns (going too far down)
+                accum += grid[i-1][j]
+            if j < n: # rows (going too far right)
+                accum += grid[i][j-1]
             return accum
         
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
+        for i in range(m): # rows (going down)
+            for j in range(n): # columns (going right)
                 grid[i][j] = checkPaths(i, j)
 
-        return grid[0][0]
+        print(grid)
+        return grid[-1][-1] # print the finish cell value
