c3d3621e6ba7fdc41247d6e295b35df4024dad1b
Will Starling <willstar2003@gmail.com>
2023-10-19 23:25:29 -0400

    C++ Solution
            Submission Statistics:
            Question #: 70
            Language: C++
            Runtime: 2 ms
            Runtime percentile: 47.38
            Memory: 6.44 MB
            Memory percentile: 31.21

--- 70. Climbing Stairs/Solution.cpp
+++ 70. Climbing Stairs/Solution.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> stairs{1, 2};
+        // climbStairs(1) = 1
+        // climbStairs(2) = 2
+        vector<int> stairs{1, 2}; // initialize a vector with values 1 and 2/ terms 1 and 2
 
-        for (int i = 3; i <=n; ++i){
-            stairs.push_back(stairs[i-2] + stairs[i-3]);
+        for (int i = 3; i <=n; ++i){ // for all terms >= 3
+            stairs.push_back(stairs[i-2] + stairs[i-3]); // add to the vector the sum of the last 2 values in the series
         }
 
-        return stairs[n-1];
+        return stairs[n-1]; // return climbStairs(n) (n-1 because array is zero-indexed)
     }
 };
