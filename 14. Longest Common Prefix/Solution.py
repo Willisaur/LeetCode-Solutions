108a07c48d821325dd86dd23454c3598f4e56670
Will Starling <willstar2003@gmail.com>
2023-12-20 02:28:28 -0600

    Python3 Solution
            Submission Statistics:
            Question #: 14
            Language: Python3
            Runtime: 33 ms
            Runtime percentile: 94.29
            Memory: 17.632 MB
            Memory percentile: 20.00

---
+++ 14. Longest Common Prefix/Solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        class TrieNode:
+            def __init__(self):
+                self.children = dict()
+        class Trie:
+            def __init__(self):
+                self.root = TrieNode()
+            def insert(self, word):
+                currentNode = self.root
+                for ch in word:
+                    if ch not in currentNode.children:
+                        currentNode.children[ch] = TrieNode()
+                    currentNode = currentNode.children[ch] 
+            def insert_MIC(self, word):
+                count = 0
+                currentNode = self.root
+                for ch in word:
+                    if ch not in currentNode.children:
+                        return count
+                    currentNode = currentNode.children[ch]
+                    count += 1
+                return count
+        
+        trie = Trie()
+        trie.insert(strs[0])
+        mostInCommon = len(strs[0])
+        for s in strs[1:]:
+            mostInCommon = min(mostInCommon, trie.insert_MIC(s))
+        return strs[0][:mostInCommon]
