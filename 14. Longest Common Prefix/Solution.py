44d1b29a51461c2f01563e25cf9f4081b6840b06
Will Starling <willstar2003@gmail.com>
2023-12-20 03:05:03 -0600

    Python3 Solution
            Submission Statistics:
            Question #: 14
            Language: Python3
            Runtime: 48 ms
            Runtime percentile: 17.51
            Memory: 17.752 MB
            Memory percentile: 20.00

--- 14. Longest Common Prefix/Solution.py
+++ 14. Longest Common Prefix/Solution.py
@@ -3,16 +3,19 @@ class Solution:
         class TrieNode:
             def __init__(self):
                 self.children = dict()
+
         class Trie:
             def __init__(self):
                 self.root = TrieNode()
+
             def insert(self, word):
                 currentNode = self.root
                 for ch in word:
                     if ch not in currentNode.children:
                         currentNode.children[ch] = TrieNode()
                     currentNode = currentNode.children[ch] 
-            def insert_MIC(self, word):
+
+            def search(self, word):
                 count = 0
                 currentNode = self.root
                 for ch in word:
@@ -22,9 +25,12 @@ class Solution:
                     count += 1
                 return count
         
+
         trie = Trie()
         trie.insert(strs[0])
         mostInCommon = len(strs[0])
+
         for s in strs[1:]:
-            mostInCommon = min(mostInCommon, trie.insert_MIC(s))
+            mostInCommon = min(mostInCommon, trie.search(s))
+
         return strs[0][:mostInCommon]
