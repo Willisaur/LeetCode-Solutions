7d77d55e94e98d9ba6dcc7d8b201bc44fbd5a7a1
Will Starling <willstar2003@gmail.com>
2023-12-27 19:53:53 -0600

    Python3 Solution
            Submission Statistics:
            Question #: 303
            Language: Python3
            Runtime: 159 ms
            Runtime percentile: 35.82
            Memory: 21.156 MB
            Memory percentile: 6.53

--- 303. Range Sum Query - Immutable/Solution.py
+++ 303. Range Sum Query - Immutable/Solution.py
@@ -1,14 +1,21 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
+        # ex: [1, 2, 3, 4, 5]
+        # tr: [1, 3, 6, 10, 15]
+        # (2, 5) = 15-6 = 9
+        # this is accum[right]-accum[left-1] for all left > 0
+        # if left == 0, return accum[right]
+        self.accum = []
+        if nums:
+            self.accum.append(nums[0])
+        for n in nums[1:]:
+            self.accum.append(n+self.accum[-1])
 
     def sumRange(self, left: int, right: int) -> int:
-        accum = 0
-        while left <= right:
-            accum += self.nums[left]
-            left += 1
-        return accum
+        if left == 0:
+            return self.accum[right]
+        return self.accum[right]-self.accum[left-1]
 
 
 # Your NumArray object will be instantiated and called as such:
