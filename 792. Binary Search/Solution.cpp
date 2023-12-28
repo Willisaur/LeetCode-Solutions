f0be156c998f1ce4e0962b74caa9185996440da0
Will Starling <willstar2003@gmail.com>
2023-12-27 23:56:24 -0600

    C++ Solution
            Submission Statistics:
            Question #: 792
            Language: C++
            Runtime: 54 ms
            Runtime percentile: 6.53
            Memory: 27.948 MB
            Memory percentile: 24.82

--- 792. Binary Search/Solution.cpp
+++ 792. Binary Search/Solution.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int i = 0; // start of the list
-        int j = nums.size() -1; // end of the list
-        int k = j / 2;
+        int i = 0;
+        int j = nums.size()-1;
+        int k = nums.size() / 2;
 
         while (i <= j){
             if (nums[k] == target){
                 return k;
             }
-            else if (nums[k] < target){
+            if (nums[k] < target)
                 i = k+1;
-            } else {
+            else 
                 j = k-1;
-            }
-            k = (i + j) / 2;
+            k = (i+j)/2;
         }
+
         return -1;
     }
 };
