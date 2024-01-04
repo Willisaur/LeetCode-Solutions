dc75b08719de69442215974de1e00d151e4d0dde
Will Starling <willstar2003@gmail.com>
2024-01-04 02:15:53 -0600

    C++ Solution
            Submission Statistics:
            Question #: 225
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 7.376 MB
            Memory percentile: 27.92

---
+++ 225. Implement Stack using Queues/Solution.cpp
@@ -0,0 +1,44 @@
+class MyStack {
+public:
+    queue<int> s; // [1, 2, 3, 4, 5]
+
+    MyStack() {}
+    
+    void push(int x) {
+        s.push(x);
+    }
+    
+    int pop() {
+        for (int i = s.size()-1; i>0; --i){ // rotate the queue until element to-be-popped is at the front
+            s.push(s.front());
+            s.pop();
+        }
+        int temp = s.front();
+        s.pop();
+        return temp;
+    }
+    
+    int top() {
+        for (int i = s.size()-1; i>0; --i){ // rotate the queue until element to-be-peeked is at the front
+            s.push(s.front());
+            s.pop();
+        }
+        int temp = s.front();
+        s.pop();
+        s.push(temp); // put it back in the queue
+        return temp;
+    }
+    
+    bool empty() {
+        return s.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
