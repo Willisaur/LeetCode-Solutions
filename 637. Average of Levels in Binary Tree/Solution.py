abf0e988a4fa782278dc7e3a50c4f6061ad6f5ac
Will Starling <willstar2003@gmail.com>
2024-01-24 15:54:01 -0500

    Python3 Solution
            Submission Statistics:
            Question #: 637
            Language: Python3
            Runtime: 50 ms
            Runtime percentile: 52.94
            Memory: 18.292 MB
            Memory percentile: 99.91

---
+++ 637. Average of Levels in Binary Tree/Solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        queue = deque([root] if root else [])
+        result = []
+
+        while queue:
+            queue_size = len(queue)
+            level_sum = 0
+            for i in range(queue_size):
+                node = queue.popleft()
+                level_sum += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level_sum/1.0/queue_size)
+        return result
