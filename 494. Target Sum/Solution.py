a0c9e1f3d749dfbd3bb3a2ec57a96e3c4a6d07e6
Will Starling <willstar2003@gmail.com>
2024-01-31 00:20:59 -0500

    Python3 Solution
            Submission Statistics:
            Question #: 494
            Language: Python3
            Runtime: 243 ms
            Runtime percentile: 40.16
            Memory: 17.452 MB
            Memory percentile: 74.73

--- 494. Target Sum/Solution.py
+++ 494. Target Sum/Solution.py
@@ -7,16 +7,17 @@ class Solution:
         # solution
         if i == len(nums)-1 and accum == target:
             return 1
-        elif i < len(nums)-1:
-            # choices
+        
+        # choices
+        if i < len(nums)-1:
             self.dp[(i, accum)] = self.findSolutions(i+1, accum+nums[i], nums, target) + self.findSolutions(i+1, accum-nums[i], nums, target)
-        else:
-            return 0
+            return self.dp[(i, accum)]
+            
+        # out of bounds or no solution
+        return 0
 
-        # no solution
-        return self.dp[(i, accum)]
 
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        self.dp = {}
+        self.dp = {} 
         return self.findSolutions(-1, 0, nums, target)
 
