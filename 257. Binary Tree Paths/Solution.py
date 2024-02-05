93b7c1e569490fda14b7e54769d08a54d27031c2
Will Starling <willstar2003@gmail.com>
2024-02-05 17:37:30 -0500

    Python3 Solution
            Submission Statistics:
            Question #: 257
            Language: Python3
            Runtime: 43 ms
            Runtime percentile: 28.92
            Memory: 16.58 MB
            Memory percentile: 64.65

--- 257. Binary Tree Paths/Solution.py
+++ 257. Binary Tree Paths/Solution.py
@@ -8,16 +8,20 @@ class Solution:
     def isLeaf(self, node):
         return not (node.left or node.right)
     def findSolutions(self, node, currentPath, paths):
+        # solution
         if self.isLeaf(node):
             paths.append(currentPath)
             return
+        # look deeper; use a choice
         if node.left:
-            self.findSolutions(node.left, f"{currentPath}->{str(node.left.val)}", paths)
+            self.findSolutions(node.left, f"{currentPath}->{node.left.val}", paths)
+        # look deeper; use a choice
         if node.right:
-            self.findSolutions(node.right, f"{currentPath}->{str(node.right.val)}", paths)
+            self.findSolutions(node.right, f"{currentPath}->{node.right.val}", paths)
+        # go back
         return
+
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         paths = []
         self.findSolutions(root, f"{root.val}", paths)
         return paths
-        
