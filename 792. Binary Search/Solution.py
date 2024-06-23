fe20ba1d3bae5f37f4b4757cab466ec140799b4f
Will Starling <willstar2003@gmail.com>
2024-06-22 17:14:54 -0700

    Python3 Solution
            Submission Statistics:
            Question #: 792
            Language: Python3
            Runtime: 180 ms
            Runtime percentile: 95.81
            Memory: 18.264 MB
            Memory percentile: 11.72

--- 792. Binary Search/Solution.py
+++ 792. Binary Search/Solution.py
@@ -1,15 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        i = 0
-        j = len(nums) - 1
-        k = j // 2
+        i, j = 0, len(nums)-1
 
-        while i <= j:
-            if target == nums[k]:
-                return k # found, return index
-            elif target < nums[k]:
-                j = k-1 # move upper pointer down
-            else:
-                i = k+1 # move lower pointer up
+        while i<=j:
             k = (i+j)//2
-        return -1 # not found, return -1
+            if nums[k] < target:
+                i = k+1
+            elif nums[k] > target:
+                j = k-1
+            elif nums[k] == target:
+                return k
+        return -1
