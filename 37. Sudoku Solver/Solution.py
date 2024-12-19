e72b654c5904b2422fa2069586342c12ebbe2ff4
Will Starling <willstar2003@gmail.com>
2024-12-19 15:23:09 -0500

    Python3 Solution
            Submission Statistics:
            Question #: 37
            Language: Python3
            Runtime: 1180 ms
            Runtime percentile: 17.14
            Memory: 17.916 MB
            Memory percentile: 6.03

--- 37. Sudoku Solver/Solution.py
+++ 37. Sudoku Solver/Solution.py
@@ -1,38 +1,35 @@
 class Solution:
-    def backtrack(self, board, cq, rm, cm, bm, i, j):
-        # find location of next blank
-        # print(f'cq: {cq}')
-        nextCell = cq.popleft()
-
-        # find possible digits
-        # try another digit in current cell and move to next cell
-        boxIndex = i//3*3 + j//3
-        for k in range(9):
-            if rm[i][k] == 0 and cm[j][k] == 0 and bm[boxIndex][k] == 0:
-                # attempt to solve using k
-                # print(f'trying {k+1} in {i} {j}')
-                rm[i][k] = 1 
-                cm[j][k] = 1
-                bm[boxIndex][k] = 1
-        
-                board[i][j] = str(k+1)
-                if nextCell == [-1, -1] or self.backtrack(board, cq, rm, cm, bm, nextCell[0], nextCell[1]):
-                    return True
-                
-                # undo work in current cell
-                board[i][j] = '.'
-                rm[i][k] = 0
-                cm[j][k] = 0
-                bm[i//3*3 + j//3][k] = 0
-        cq.appendleft(nextCell)
-        
-        # undo work in previous cell
-        return False
-    
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
+        def backtrack(board, cq, rm, cm, bm, i, j):
+            # find location of next blank
+            # print(f'cq: {cq}')
+            nextCell = cq.popleft()
+
+            # find possible digits
+            # try another digit in current cell and move to next cell
+            boxIndex = i//3*3 + j//3
+            for k in range(9):
+                if rm[i][k] == 0 and cm[j][k] == 0 and bm[boxIndex][k] == 0:
+                    # attempt to solve using k
+                    # print(f'trying {k+1} in {i} {j}')
+                    rm[i][k] = 1 
+                    cm[j][k] = 1
+                    bm[boxIndex][k] = 1
+            
+                    board[i][j] = str(k+1)
+                    if nextCell == [-1, -1] or backtrack(board, cq, rm, cm, bm, nextCell[0], nextCell[1]):
+                        return True
+                    
+                    # undo work in current cell
+                    board[i][j] = '.'
+                    rm[i][k] = 0
+                    cm[j][k] = 0
+                    bm[boxIndex][k] = 0
+            cq.appendleft(nextCell)
+            
+            # undo work in previous cell
+            return False
+
         cellQueue = deque([])
         rowMemo = [[0 for _ in range(9)] for _ in range(9)]
         colMemo = [[0 for _ in range(9)] for _ in range(9)]
@@ -49,5 +46,5 @@ class Solution:
         cellQueue.append([-1, -1])
         
         start = cellQueue.popleft()
-        self.backtrack(board, cellQueue, rowMemo, colMemo, boxMemo, start[0], start[1])
+        backtrack(board, cellQueue, rowMemo, colMemo, boxMemo, start[0], start[1])
 
