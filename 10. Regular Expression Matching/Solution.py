5058b174e360ba9912617d2cfe8fca220bd349d4
Will Starling <willstar2003@gmail.com>
2024-12-27 02:43:30 -0500

    Python3 Solution
            Submission Statistics:
            Question #: 10
            Language: Python3
            Runtime: 8832 ms
            Runtime percentile: 6.79
            Memory: 17.736 MB
            Memory percentile: 14.35

---
+++ 10. Regular Expression Matching/Solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        if s == '' and p == '':
+            return True
+        elif p == '':
+            return False
+
+        if len(p) > 1 and p[1] == '*':
+            # found 0 times
+            if self.isMatch(s, p[2:]):
+                return True
+            # found 1 or more times
+            if len(s):
+                i = 0
+                while i < len(s) and p[0] in [s[i], '.']:
+                    if self.isMatch(s[i+1:], p[2:]):
+                        return True
+                    i += 1
+        elif len(s) and p[0] in [s[0], '.']:
+            if self.isMatch(s[1:], p[1:]):
+                return True
+        
+        return False
