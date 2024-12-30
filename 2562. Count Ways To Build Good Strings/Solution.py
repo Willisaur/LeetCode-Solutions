4d88a3e91d4737ab3a2fdfe3501c34dd3da3dc39
Will Starling <willstar2003@gmail.com>
2024-12-30 01:52:52 -0500

    Python3 Solution
            Submission Statistics:
            Question #: 2562
            Language: Python3
            Runtime: 983 ms
            Runtime percentile: 18.36
            Memory: 679.108 MB
            Memory percentile: 8.39

---
+++ 2562. Count Ways To Build Good Strings/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        dp = [0] * (high+1)
+        dp[0] = 1
+
+        accum = 0
+        for i in range(min(zero, one), high+1):
+            if i - zero >= 0:
+                dp[i] += dp[i - zero]
+            if i - one >= 0:
+                dp[i] += dp[i - one]
+            if i >= low:
+                accum += dp[i]
+
+        # print(dp)
+        return accum % (10**9 + 7)
+
+
