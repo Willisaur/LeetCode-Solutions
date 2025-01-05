90c24cdf893a7a58a369373c5d78a24949ba6184
Will Starling <willstar2003@gmail.com>
2025-01-05 17:05:35 -0600

    Python3 Solution
            Submission Statistics:
            Question #: 2465
            Language: Python3
            Runtime: 3605 ms
            Runtime percentile: 5.11
            Memory: 41.332 MB
            Memory percentile: 30.93

---
+++ 2465. Shifting Letters II/Solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        arr = [0] * len(s)
+        out = []
+        
+        def shiftLetter(ch, amnt):
+            print(ch, amnt)
+            if amnt > 0:
+                while ord(ch) + amnt > 97 + 25:
+                    amnt -= 26
+                print(chr(ord(ch) + amnt))
+                return chr(ord(ch) + amnt)
+            if amnt < 0:
+                while ord(ch) + amnt < 97:
+                    amnt += 26
+                print(chr(ord(ch) + amnt))
+                return chr(ord(ch) + amnt)
+            return ch
+        
+        for i in range(len(shifts)):
+            shift = shifts[i]
+            arr[shift[0]] += 1 if shift[-1]==1 else -1
+            if shift[1]+1 < len(s):
+                arr[shift[1]+1] -= 1 if shift[-1] == 1 else -1
+        
+        print(arr)
+        for i in range(len(s)):
+            if i > 0:
+                arr[i] += arr[i-1]
+            out.append(shiftLetter(s[i], arr[i]))
+
+        return ''.join(out)
