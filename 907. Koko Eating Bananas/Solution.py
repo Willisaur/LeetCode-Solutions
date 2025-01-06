5f73053633bf7f9d6671e85a7dcdbafdd48d8df6
Will Starling <willstar2003@gmail.com>
2025-01-05 18:32:41 -0600

    Python3 Solution
            Submission Statistics:
            Question #: 907
            Language: Python3
            Runtime: 155 ms
            Runtime percentile: 57.65
            Memory: 19.268 MB
            Memory percentile: 9.58

---
+++ 907. Koko Eating Bananas/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # if h == len(piles):
+        #     return max(piles)
+        
+        def eat(arr, amnt):
+            # print(amnt, [math.ceil(p / amnt) for p in piles])
+            return sum([math.ceil(p / amnt) for p in piles]) <= h
+        
+        lower, upper = 1, max(piles)
+        while lower < upper:
+            middle = (lower + upper)//2
+            # print(lower, upper, middle)
+            if eat(piles.copy(), middle): # may be eating too many (k is too large)
+                upper = middle
+            else: # insufficient time for given problem (k is too small)
+                lower = middle + 1
+        return lower
