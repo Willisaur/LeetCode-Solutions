aa2863834d1f37ac9ce0e830b5940de944e857c0
Will Starling <willstar2003@gmail.com>
2025-05-18 23:35:14 -0700

    Python3 Solution
            Submission Statistics:
            Question #: 3321
            Language: Python3
            Runtime: 1 ms
            Runtime percentile: 12.37
            Memory: 17.62 MB
            Memory percentile: 80.27

---
+++ 3321. Type of Triangle/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        a, b, c = sorted(nums)
+        if a + b <= c:
+            return "none"
+        elif a == c:
+            return "equilateral"
+        elif a == b or b == c:
+            return "isosceles"
+        else:
+            return "scalene"
