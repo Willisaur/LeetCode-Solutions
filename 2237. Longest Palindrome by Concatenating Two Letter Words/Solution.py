8c757c63da0e91eb0e4e56a79cc465094fce0cc2
Will Starling <willstar2003@gmail.com>
2025-05-24 21:55:55 -0700

    Python3 Solution
            Submission Statistics:
            Question #: 2237
            Language: Python3
            Runtime: 55 ms
            Runtime percentile: 65.16
            Memory: 35.612 MB
            Memory percentile: 40.25

---
+++ 2237. Longest Palindrome by Concatenating Two Letter Words/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        counts = Counter(words)
+        length = 0
+        print(counts)
+        flag = True
+        for k, v in counts.items():
+            print(k, v)
+            k2 = k[::-1]
+            if k != k2:
+                v2 = counts.get(k2, 0)
+                length += 2 * min(v, v2)
+            else:
+                if v % 2 == 0:
+
+                    length += 2 * v
+                elif v > 1:
+                    length += 2 * (v-1)
+                    
+                if flag and v % 2:
+                    flag = False
+                    length += 2
+        
+        return length
