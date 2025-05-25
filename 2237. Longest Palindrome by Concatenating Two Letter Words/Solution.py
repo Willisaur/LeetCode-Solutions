a94df29341ca7832ba5829675256f8c9cf68c478
Will Starling <willstar2003@gmail.com>
2025-05-24 22:02:22 -0700

    Python3 Solution
            Submission Statistics:
            Question #: 2237
            Language: Python3
            Runtime: 43 ms
            Runtime percentile: 88.27
            Memory: 35.656 MB
            Memory percentile: 40.25

--- 2237. Longest Palindrome by Concatenating Two Letter Words/Solution.py
+++ 2237. Longest Palindrome by Concatenating Two Letter Words/Solution.py
@@ -9,16 +9,16 @@ class Solution:
             # print(k, v)
             k2 = k[::-1]
             
-            if k != k2:
+            if k != k2: # ex: ab matches with ba
                 v2 = counts.get(k2, 0)
-                length += 2 * min(v, v2)
+                length += 2 * min(v, v2) # ex: ab counted this time, ba counted later
             else:
-                if v % 2 == 0:
+                if v % 2 == 0: # symmetric; add as-is
                     length += 2 * v
-                elif v > 1:
+                elif v > 1: # odd number of symmetric occurences -- just remove the odd one
                     length += 2 * (v-1)
                     
-                if flag and v % 2:
+                if flag and v % 2: # single item in center of palindrome
                     flag = False
                     length += 2
         
