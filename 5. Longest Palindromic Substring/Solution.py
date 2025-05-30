975e9a3b22a9e72419184ff5bbba89f558c3bd02
Will Starling <willstar2003@gmail.com>
2025-05-29 22:42:19 -0700

    Python3 Solution
            Submission Statistics:
            Question #: 5
            Language: Python3
            Runtime: 9114 ms
            Runtime percentile: 5.00
            Memory: 17.672 MB
            Memory percentile: 96.09

---
+++ 5. Longest Palindromic Substring/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest = s[0]
+
+        for i in range(len(s)):
+            for j in range(len(s)-1, i, -1):
+                a = s[i:j+1]
+                b = s[j:i-1:-1] if i-1 != -1 else s[j::-1]
+                # print(i,j)
+                # print(a, '|', b)
+                if a == b and len(a) > len(longest):
+                    longest = a
+
+        return longest
